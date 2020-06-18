e5875d18c98c04d3248912c8ab2766270910b4fb
VibhuRajput <39965391+VibhuRajput@users.noreply.github.com>
2020-06-18 16:57:57 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,36 @@
+state={
+    fields: [
+        {
+            placeholder: "Purpose Of E-commerce",
+            name: "name",
+            input_type: "Dropdown",
+            required: [
+                "Shirts",
+                "Phone Case",
+                "Bike",
+                "Handbag",
+                "Furniture",
+                "Others"
+            ]
+        },
+
+        {
+            placeholder: "Name",
+            name: "remaining_values",
+            input_type: "big_text",
+            required: true
+        },
+        {
+            placeholder: "email",
+            name: "email",
+            input_type: "text",
+            required: true
+        },
+        {
+            placeholder: "Preferred time to call you",
+            name: "contact_time",
+            input_type: "text",
+            required: true
+        }
+    ]
+}

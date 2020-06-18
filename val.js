d95a68fac258f3e647a8495f153d1b05a5883179
VibhuRajput <39965391+VibhuRajput@users.noreply.github.com>
2020-06-18 16:55:05 +0530

    Create val.js

---
+++ val.js
@@ -0,0 +1,12 @@
+import React from "react";
+const DropdownSelect = ({ name, placeholder, required, val, _handleChange}) => (
+<div>
+<label>{placeholder}</label>
+<select name ={name} required={required} onChange={_handleChange}>
+    <option value="">Select and option</option>
+    {val.map(values =><option value={values} key={values}>{values}</option>)}
+
+</select>
+</div>
+);
+export default DropdownSelect;
